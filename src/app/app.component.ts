c5c0ef6ca686cb71bf62c9779379fd8d2a83fb42
Tyler Cutler <mrtcutler@gmail.com>
2022-02-21 21:05:02 -0500

    big update to convert tasks to firestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { CdkDragDrop, transferArrayItem } from "@angular/cdk/drag-drop";
 import { Component } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/compat/firestore";
 import { MatDialog } from "@angular/material/dialog";
+import { Observable } from "rxjs";
 import { TaskDialogResult } from "./task-dialog/interface/task-dialog-result.interface";
 import { TaskDialogComponent } from "./task-dialog/task-dialog.component";
 import { Task } from "./task/models/task.model";
@@ -11,21 +13,13 @@ import { Task } from "./task/models/task.model";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  public inProgress: Task[] = [];
-  public done: Task[] = [];
-  public todo: Task[] = [
-    {
-      title: "Buy Milk",
-      description: "Go to the store and buy milk",
-    },
-    {
-      title: "Create Kanban App",
-      description: "Using Firebase and Angular to create a Kanban App!",
-    }
-  ];
+  public done = this.store.collection('done').valueChanges({ idField: 'id'}) as Observable<Task[]>;
+  public inProgress = this.store.collection('inProgress').valueChanges({ idField: 'id'}) as Observable<Task[]>;
+  public todo = this.store.collection('todo').valueChanges({ idField: 'id'}) as Observable<Task[]>;
 
   constructor(
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private store: AngularFirestore
   ) {
     this.dialog = dialog;
   }
@@ -40,24 +34,51 @@ export class AppComponent {
     dialogRef
       .afterClosed()
       .subscribe((result: TaskDialogResult) => {
-        if (!result) {
+        if (!result || (!!result && !result.task?.title)) {
           return;
         }
-        this.todo.push(result.task);
+        this.store.collection('todo').add(result.task);
       })
   }
 
-  public editTask(list: string, task: Task): void {
-
+  public editTask(list: 'done' | 'todo' | 'inProgress', task: Task): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task,
+        enableDelete: true,
+      },
+    });
+    dialogRef.afterClosed()
+      .subscribe((result: TaskDialogResult|undefined) => {
+        if (!result) {
+          return;
+        }
+        if (result.delete) {
+          this.store.collection(list).doc(task.id).delete();
+        } else {
+          this.store.collection(list).doc(task.id).update(task);
+        }
+      });
   }
 
-  public drop(event: CdkDragDrop<Task[]>): void {
+  public drop(event: CdkDragDrop<Task[]> | any): void {
     if (event.previousContainer === event.container) {
       return;
     }
     if (!event.container?.data || !event.previousContainer?.data) {
       return;
     }
+
+    const item = event.previousContainer.data[event.previousIndex];
+    this.store.firestore.runTransaction(() => {
+      const promise = Promise.all([
+        this.store.collection(event.previousContainer.id).doc(item.id).delete(),
+        this.store.collection(event.container.id).add(item)
+      ]);
+      return promise;
+    });
+
     transferArrayItem(
       event.previousContainer.data,
       event.container.data,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'kanban-board-2256e',
+    appId: '1:810471447947:web:57d53f304f4483d0e8638e',
+    storageBucket: 'kanban-board-2256e.appspot.com',
+    apiKey: 'AIzaSyCUjI5vuYfmYOD04LL9ovTmjr9x1xDG5TE',
+    authDomain: 'kanban-board-2256e.firebaseapp.com',
+    messagingSenderId: '810471447947',
+    measurementId: 'G-4ERJ3EG6KZ',
+  },
   production: false
 };
 
